dc630c4a84dff361a1081008ad401df961e406df
AkhilChandran <akhil@trycle.com>
2026-06-20 16:20:36 +0530

    add get update done

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (dbConnect) {
 }
 
 http
-  .createServer((req, res) => {
+  .createServer(async (req, res) => {
     if (req.url === "/api/students" && req.method === "POST") {
       console.log("api/students call successfull");
       let body = "";
@@ -41,8 +41,57 @@ http
       });
 
       req.on("end", async () => {
-        const inputData = querystring.parse(body);
-        const studentData = inputData;
+        const studentData = JSON.parse(body);
+
+        try {
+          await studentCollection.insertOne(studentData);
+          console.log("student data saved to MongoDB");
+          res.writeHead(200, "Successfull");
+        } catch (error) {
+          console.log("student data not saved to MongoDB");
+          res.writeHead(500, "Not Successfull");
+        }
+      });
+    } else if (req.url === "/api/students" && req.method === "GET") {
+      const dataFetched = await studentCollection.find().toArray();
+
+      res.end(JSON.stringify(dataFetched));
+
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end("Student details fetched successfully");
+    } else if (
+      req.method === "PUT" &&
+      req.url.startsWith("/api/students?id=")
+    ) {
+      //   console.log("I ame here");
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      req.on("end", async () => {
+        try {
+          const url = new URL(req.url, `http://${req.headers.host}`);
+          const id = url.searchParams.get("id");
+
+          console.log(id);
+          const updatedData = JSON.parse(body);
+
+          console.log(updatedData);
+          const supdatedData = await studentCollection.updateOne(
+            { Id: id },
+            { $set: updatedData },
+          );
+
+          console.log(supdatedData);
+
+          res.writeHead(200, { "content-type": "application/json" });
+          res.end("Student updated successfully");
+        } catch (error) {
+          res.writeHead(500, { "content-type": "application/json" });
+          res.end("Student Not updated successfully");
+        }
       });
     }
   })
